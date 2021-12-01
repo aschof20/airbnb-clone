82785bdaa184b5b9769ca95b771f016b900aa437
Alice Schofield <a.v.schofield@gmail.com>
2021-12-01 17:00:39 +1000

    Finished the Header/Navbar

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import { SearchIcon } from '@heroicons/react/solid';
+import { SearchIcon, GlobeAltIcon, MenuIcon, UserCircleIcon, UsersIcon } from '@heroicons/react/solid';
 
 function Header() {
     return (
@@ -25,11 +25,17 @@ function Header() {
             </div>
 
             {/* Right header section */}
-            <div>
+            <div className="flex space-x-4 items-center justify-end text-gray-500">
+                <p className="hidden md:inline cursor-pointer">Become a Host</p>
+                <GlobeAltIcon className="h-6 cursor-pointer" />
+                <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+                    <MenuIcon className="h-6" />
+                    <UserCircleIcon className="h-6" />
 
+                </div>
             </div>
         </header>
     );
 }
 
-export default Header;
+export default Header;;
